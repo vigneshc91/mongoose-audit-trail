79f60ea61de29a0d932fd7838b0b5e2c93dc7ef3
Vignesh C <vigneshc@wekancode.com>
2020-07-23 17:04:15 +0530

    hooks modified to save history in async mode

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    semi: true,
+    singleQuote: true,
+    printWidth: 250,
+    useTabs: false,
+    tabWidth: 4,
+    bracketSpacing: true,
+    arrowParens: 'always',
+  };
+  
